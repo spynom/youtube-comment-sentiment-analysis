30bab92802b39f374c213f993a996679ce175051
spynom <saurav24082000@gmail.com>
2024-10-22 22:12:49 +0530

    added logs and exceptional handling in src/model_building.py

--- src/model_building.py
+++ src/model_building.py
@@ -5,38 +5,111 @@ import yaml
 import pickle
 from sklearn.metrics import classification_report
 import json
+import logging
 
-with open("params.yaml") as f:
-    params = yaml.safe_load(f) ["model_building"]
+logger = logging.getLogger("Feature Engineering")
+logger.setLevel(logging.DEBUG)
 
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+
+try:
+    with open("params.yaml") as f:
+        params = yaml.safe_load(f) ["model_building"]
+    logger.info("params yaml file is read successfully")
+
+except FileNotFoundError:
+    logger.error("params yaml file not found")
+    raise
+except yaml.YAMLError as exc:
+    logger.error(f"error in params.yaml file format: {exc}")
+    raise
+except Exception as e:
+    logger.error(e)
+    raise
 final_data_dir = os.path.join("data", "final")
 
-train_dataset = pd.read_csv (os.path.join(final_data_dir, "train.csv"))
-test_dataset = pd.read_csv (os.path.join(final_data_dir, "test.csv"))
+try:
+    train_dataset = pd.read_csv (os.path.join(final_data_dir, "train.csv"))
+    logger.info("train dataset read successfully")
 
-with open(os.path.join("models","transformer.pkl"), "rb") as f:
-    transformer=pickle.load(f)
+except FileNotFoundError as e:
+    logger.error("train dataset file not found")
+    raise
+except Exception as e:
+    logger.error(e)
+    raise
+try:
+    test_dataset = pd.read_csv (os.path.join(final_data_dir, "test.csv"))
+    logger.info("test dataset read successfully")
 
+except FileNotFoundError as e:
+    logger.error("test dataset file not found")
+    raise
+except Exception as e:
+    logger.error(e)
+    raise
+try:
+    with open(os.path.join("models","transformer.pkl"), "rb") as f:
+        transformer=pickle.load(f)
+
+except FileNotFoundError as e:
+    logger.error("transformer pkl not found")
+    raise
+except pickle.PickleError as e:
+    logger.error(f"error in pkl file: {e}")
+    raise
+except Exception as e:
+    logger.error(e)
+    raise
 X_train,y_train,X_test,y_test = train_dataset.iloc[:,:-1], train_dataset.iloc[:,-1], test_dataset.iloc[:,:-1], test_dataset.iloc[:,-1]
 
-model = LGBMClassifier(
-    **params["model_parameters"]
-)
+try:
+    model = LGBMClassifier(
+        **params["model_parameters"]
+    )
+except Exception as e:
+    logger.error(e)
+    raise
 
 model.fit (X_train, y_train)
+logger.info("model fit successfully")
 
 y_train_hat = model.predict(X_train)
 y_test_hat = model.predict(X_test)
 
-with open(os.path.join("reports","evaluate_matrices.json"),"w") as f:
-    report={
-        "train": classification_report(y_train,y_train_hat,output_dict=True),
-        "test": classification_report(y_test,y_test_hat,output_dict=True)
-    }
-    json.dump(report,f)
+try:
+    with open(os.path.join("reports","evaluate_matrices.json"),"w") as f:
+        report={
+            "train": classification_report(y_train,y_train_hat,output_dict=True),
+            "test": classification_report(y_test,y_test_hat,output_dict=True)
+        }
+        json.dump(report,f)
+    logger.info("evaluation report saved successfully")
 
-with open(os.path.join("models","final_model.pkl"),"wb") as f:
-    pickle.dump(model,f)
+except FileNotFoundError as e:
+    logger.error(e)
+    raise
+except Exception as e:
+    logger.error(e)
+    raise
+
+try:
+    with open(os.path.join("models","final_model.pkl"),"wb") as f:
+        pickle.dump(model,f)
+    logger.info("model saved successfully")
+
+except FileNotFoundError as e:
+    logger.error(e)
+    raise
+except Exception as e:
+    logger.error(e)
 
 
 
