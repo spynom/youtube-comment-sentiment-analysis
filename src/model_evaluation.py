d18632a8cc2b29f94acaef0565108e0a9a57819f
spynom <saurav24082000@gmail.com>
2024-10-23 20:54:11 +0530

    try to fix cl pipeline

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -19,7 +19,7 @@ load_dotenv()
 # Set up DagsHub credentials for MLflow tracking
 dagshub_token = os.getenv("DAGSHUB_TOKEN")
 if not dagshub_token:
-    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+    raise EnvironmentError("DAGSHUB_TOKEN environment variable is not set")
 
 os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
