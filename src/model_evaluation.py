045a3f6cea853efb82fe0dfc9e9d66337547690e
spynom <saurav24082000@gmail.com>
2024-10-23 21:40:30 +0530

    test5 to fix enviroment variable problem

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -20,7 +20,9 @@ import dagshub
 dagshub_token = os.getenv("DAGSHUB_TOKEN")
 print(dagshub_token)
 if not dagshub_token:
-    raise EnvironmentError("DAGSHUB_TOKEN environment variable is not set")
+    dagshub_token = os.getenv("DAGSHUB")
+    if not dagshub_token:
+        raise EnvironmentError("DAGSHUB_TOKEN environment variable is not set")
 
 os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
 os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
